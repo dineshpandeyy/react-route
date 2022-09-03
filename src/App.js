08620f44846a4b1dd866a8fabea6d49ba7fa97e3
Dinesh Pandey <dineshpandey3536@gmail.com>
2022-09-02 19:41:33 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,50 @@
+
+import './App.css';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Orange from './pages/Orange';
+import Red from './pages/Red';
+import Green from './pages/Green';
+import Purple from './pages/Purple';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to color router app</h1>
+      <nav className="navbar navbar-default navbar-static-top">
+        <ul class="nav nav-pills nav-fill">
+          <li class="nav-item">
+            <Link className="nav-link" to="orange">
+              Orange
+            </Link>
+          </li>
+          <li class="nav-item">
+            <Link className="nav-link" to="red">
+              Red
+            </Link>
+          </li>
+          <li class="nav-item">
+            <Link className="nav-link" to="green">
+              Green
+            </Link>
+          </li>
+          <li class="nav-item">
+            <Link className="nav-link" to="purple">
+              Purple
+            </Link>
+          </li>
+        </ul>
+      </nav>
+
+      <div className="routes-wrapper">
+        <Routes>
+          <Route path="/orange" element={<Orange />} />
+          <Route path="/red" element={<Red />} />
+          <Route path="/purple" element={<Purple />} />
+          <Route path="/green" element={<Green />} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+export default App;
